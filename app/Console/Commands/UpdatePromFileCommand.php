7892e89dcb8169a6f59a9b3971c72c6935b8e5af
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-06 10:38:33 +0300

    initial commit.

--- app/Console/Commands/UpdatePromFileCommand.php
+++ app/Console/Commands/UpdatePromFileCommand.php
@@ -58,88 +58,87 @@ class UpdatePromFileCommand extends Command
         $data = $this->dropService->getRemoteData($url, $inputKey);
         $template = 'export-origami.xls';
 
-        dd($data[0]);
+        //dd($data[0]);
 
-//        foreach ($data as $apiProduct) {
-//            $vendor = $apiProduct['vendor'];
-//            $oneProduct = OrigamiProducts::query()
-//                ->where(
-//                    'vendorCode',
-//                    trim($apiProduct['vendorCode'])
-//                )
-//                ->where('provider', $dropProvider)
-//                ->first();
-//
-//            if ($oneProduct) {
-//                if ($apiProduct['vendor'] == "Рів'єра") {
-//                    $vendor = 'Riviera';
-//                } elseif ($apiProduct['vendor'] == "Орігамі") {
-//                    $vendor = 'Origami';
-//                }
-//
-//                if ($apiProduct['vendorCode'] == 'AL001') {
-//                    $productType = 'Акриловий лак';
-//                } else {
-//                    if ($apiProduct['productType'] == 'Інше') {
-//                        $productType = 'Розмальовка  для дітей';
-//                    } else {
-//                        $productType = $apiProduct['productType'];
-//                    }
-//                }
-//                $recommendedPrice = $apiProduct['recommendedPrice'];
-//                if ($apiProduct['price'] > $apiProduct['recommendedPrice']) {
-//                    $recommendedPrice = $apiProduct['price'];
-//                }
-//                $oneProduct->price = $apiProduct['price'];
-//                $oneProduct->recommendedPrice = $recommendedPrice;
-//                $oneProduct->vendor = $vendor;
-//                $oneProduct->productType = $productType;
-//                $oneProduct->nameUa = str_replace("&quot;", '"', $apiProduct['nameUa']);
-//                $oneProduct->nameUa = str_replace("  ", ' ', $oneProduct->nameUa);
-//
-//                $oneProduct->vendorCode = trim($apiProduct['vendorCode']);
-//                if ($oneProduct->nameUa != '' && $oneProduct->name != '') {
-//                    $oneProduct->active = 1;
-//                }
-//                $oneProduct->save();
-//            } else {
-//                OrigamiProducts::query()
-//                    ->create(
-//                        [
-//                            'vendorCode' => trim($apiProduct['vendorCode']),
-//                            'vendor' => trim($vendor),
-//                            'imageUrl' => $apiProduct['imageUrl'],
-//                            'nameUa' => str_replace(PHP_EOL, '', $apiProduct['nameUa']),
-//                            'name' => '',
-//                            'promID' => $apiProduct['promID'],
-//                            'description' => '',
-//                            'description_ua' => '',
-//                            'productType' => $apiProduct['productType'],
-//                            'size' => $apiProduct['size'],
-//                            'price' => $apiProduct['price'],
-//                            'recommendedPrice' => $apiProduct['recommendedPrice'],
-//                            'quantityInStock' => $apiProduct['quantityInStock'],
-//                            'hasHigherPrice' => $apiProduct['hasHigherPrice'],
-//                            'active' => 0,
-//                            'provider' => $dropProvider,
-//                        ]
-//                    );
-//            }
-//        }
-//
-//        $excelData = $this->dropService->getExcelData($data);
-//
-//        $spreadsheet = IOFactory::load(resource_path() . '/export_template/' . $template);
-//        $spreadsheet->getActiveSheet()->fromArray(
-//            $excelData,
-//            null,
-//            'A2'
-//        );
-//
-//        $writer = IOFactory::createWriter($spreadsheet, 'Xls');
-//        $path = '/example/' . $template;
-//        $file = public_path() . $path;
-//        $writer->save($file);
+        foreach ($data as $apiProduct) {
+            $vendor = $apiProduct['vendor'];
+            $oneProduct = OrigamiProducts::query()
+                ->where(
+                    'vendorCode',
+                    trim($apiProduct['vendorCode'])
+                )
+                ->where('provider', $dropProvider)
+                ->first();
+
+            if ($oneProduct) {
+                if ($apiProduct['vendor'] == "Рів'єра") {
+                    $vendor = 'Riviera';
+                } elseif ($apiProduct['vendor'] == "Орігамі") {
+                    $vendor = 'Origami';
+                }
+
+                if ($apiProduct['vendorCode'] == 'AL001') {
+                    $productType = 'Акриловий лак';
+                } else {
+                    if ($apiProduct['productType'] == 'Інше') {
+                        $productType = 'Розмальовка  для дітей';
+                    } else {
+                        $productType = $apiProduct['productType'];
+                    }
+                }
+                $recommendedPrice = $apiProduct['recommendedPrice'];
+                if ($apiProduct['price'] > $apiProduct['recommendedPrice']) {
+                    $recommendedPrice = $apiProduct['price'];
+                }
+                $oneProduct->price = $apiProduct['price'];
+                $oneProduct->recommendedPrice = $recommendedPrice;
+                $oneProduct->vendor = $vendor;
+                $oneProduct->productType = $productType;
+                $oneProduct->nameUa = str_replace("&quot;", '"', $apiProduct['nameUa']);
+                $oneProduct->nameUa = str_replace("  ", ' ', $oneProduct->nameUa);
+                $oneProduct->vendorCode = trim($apiProduct['vendorCode']);
+                if ($oneProduct->nameUa != '' && $oneProduct->name != '') {
+                    $oneProduct->active = 1;
+                }
+                $oneProduct->save();
+            } else {
+                OrigamiProducts::query()
+                    ->create(
+                        [
+                            'vendorCode' => trim($apiProduct['vendorCode']),
+                            'vendor' => trim($vendor),
+                            'imageUrl' => $apiProduct['imageUrl'],
+                            'nameUa' => str_replace(PHP_EOL, '', $apiProduct['nameUa']),
+                            'name' => '',
+                            'promID' => $apiProduct['promID'],
+                            'description' => '',
+                            'description_ua' => '',
+                            'productType' => $apiProduct['productType'],
+                            'size' => $apiProduct['size'],
+                            'price' => $apiProduct['price'],
+                            'recommendedPrice' => $apiProduct['recommendedPrice'],
+                            'quantityInStock' => $apiProduct['quantityInStock'],
+                            'hasHigherPrice' => $apiProduct['hasHigherPrice'],
+                            'active' => 0,
+                            'provider' => $dropProvider,
+                        ]
+                    );
+            }
+        }
+
+        $excelData = $this->dropService->getExcelData($data);
+
+        $spreadsheet = IOFactory::load(resource_path() . '/export_template/' . $template);
+        $spreadsheet->getActiveSheet()->fromArray(
+            $excelData,
+            null,
+            'A2'
+        );
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xls');
+        $path = '/example/' . $template;
+        $file = public_path() . $path;
+        $writer->save($file);
     }
 
     private function translateAi()
