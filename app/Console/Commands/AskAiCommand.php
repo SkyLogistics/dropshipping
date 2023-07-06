de8f1ba8c2d7dc9211fa519efb81e3de72530693
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-06 12:59:31 +0300

    initial commit.

--- app/Console/Commands/AskAiCommand.php
+++ app/Console/Commands/AskAiCommand.php
@@ -47,7 +47,7 @@ class AskAiCommand extends Command
                 $client = new \GuzzleHttp\Client([])
             ) // default: HTTP client found using PSR-18 HTTP Client Discovery
             ->withHttpHeader('Authorization', 'Bearer ' . $yourApiKey)
-            ->withQueryParam('OpenAI-Organization', 'org-WYlAZmxU71900K2AUOdRsw3e')
+            ->withHttpHeader('OpenAI-Organization', 'org-WYlAZmxU71900K2AUOdRsw3e')
             ->withStreamHandler(fn(RequestInterface $request): ResponseInterface => $client->send($request, [
                 'stream' => true // Allows to provide a custom stream handler for the http client.
             ]))
