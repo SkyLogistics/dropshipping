1e90e706e514c9e12200b9c20ad8b32772eafa99
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-06 09:53:38 +0300

    initial commit.

--- app/Console/Commands/UpdatePromFileCommand.php
+++ app/Console/Commands/UpdatePromFileCommand.php
@@ -14,7 +14,7 @@ class UpdatePromFileCommand extends Command
      *
      * @var string
      */
-    protected string $signature = 'update:export {provider}';
+    protected string $name = 'update:export {provider}';
 
     /**
      * The console command description.
