41cb7ff99ee9cec665ebc6a60970320218f0d521
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-09 23:52:02 +0300

    m

--- app/Console/Commands/AskAiCommand.php
+++ app/Console/Commands/AskAiCommand.php
@@ -80,7 +80,8 @@ class AskAiCommand extends Command
                             "content" => $translate
                         ],
                     ],
-                    'model' => 'gpt-3.5-turbo-16k',
+//                    'model' => 'gpt-3.5-turbo',
+                    'model' => 'text-ada-001',
                     'temperature' => 0.7,
                     'max_tokens' => 1000,
                     'frequency_penalty' => 0,
