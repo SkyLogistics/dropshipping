4d15a44174e8fbbfd035bc16931ffc51407889e2
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-10 01:05:10 +0300

    m

--- provider/royal_grab.js
+++ provider/royal_grab.js
@@ -6,7 +6,7 @@ async function update(){
     // Параметры подключения к базе данных
     const connection = mysql.createConnection({
         host: process.env.DB_HOST,
-        user: process.env.DB_PASSWORD,
+        user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
     });
