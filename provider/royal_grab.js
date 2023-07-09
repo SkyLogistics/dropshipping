b041f2b666a89fa4a59990eb19cd421f0dd379c5
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-10 01:45:22 +0300

    m

--- provider/royal_grab.js
+++ provider/royal_grab.js
@@ -3,8 +3,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 console.log(process.env.DB_USERNAME);
+let content = '';
 
-async function update(){
+await function getUrl() {
+    let myDesc = [];
     // Параметры подключения к базе данных
     const connection = mysql.createConnection({
         host: process.env.DB_HOST,
@@ -23,7 +25,7 @@ async function update(){
         console.log('Успешное подключение к базе данных MySQL');
 
         // Чтение данных из таблицы
-        const selectQuery = "SELECT * FROM origami_product where provider='royal' and description = '' LIMIT 2";
+        const selectQuery = "SELECT * FROM origami_product where provider='royal' and description = '' LIMIT 10";
         connection.query(selectQuery, (err, rows) => {
             if (err) {
                 console.error('Ошибка чтения данных: ', err);
@@ -33,8 +35,15 @@ async function update(){
                 rows.forEach((row) => {
                     const id = row.id;
                     const url = row.productUrl;
+                    //console.log('ur = ' + url)
+                    myDesc.push({"id": id, "url": url});
+                    console.log(myDesc)
                     const divContent = scrapeHTMLFromURL(url);
-                    const updateQuery = `UPDATE origami_product SET description='${divContent}' WHERE id=${id}`;
+                    console.log('divContent and id = ' + id)
+                    console.log(divContent)
+                    const updateQuery = `UPDATE origami_product
+                                         SET description='${divContent}'
+                                         WHERE id = ${id}`;
                     connection.query(updateQuery, (err, result) => {
                         if (err) {
                             console.error(`Ошибка обновления поля для записи с ID ${id}: `, err);
@@ -52,46 +61,41 @@ async function update(){
                     console.error('Ошибка закрытия соединения: ', err);
                 } else {
                     console.log('Соединение с базой данных закрыто');
+
                 }
             });
+
+
         });
     });
+
+    return myDesc;
 }
 
 async function scrapeHTMLFromURL(url) {
-    // Launch a new browser instance
     const browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             args: ['--no-sandbox'],
             downloadsPath: '/tmp'
         }
     );
 
-    // Create a new page
     const page = await browser.newPage();
-
-    // Navigate to the URL
     await page.goto(url);
-
-    // Get the HTML content of the page
     const htmlContent = await page.content();
-
-    // Find the desired div element
     const divSelector = 'div[itemprop="description"]';
     const divElement = await page.$(divSelector);
 
-    let divContent = '';
     if (divElement) {
-        // Get the innerHTML of the div element
-        divContent = await page.evaluate(element => element.innerHTML, divElement);
-        console.log(divContent);
+        content = await page.evaluate(element => element.innerHTML, divElement);
+
     } else {
         console.log('Div not found.');
     }
     // Close the browser
     await browser.close();
-
-    return divContent;
 }
 
-update().then(r => console.log('Done'));
+let productUrls = getUrl();
+
+console.log(productUrls);
