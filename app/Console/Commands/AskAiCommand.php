5de4a44b67434cd2c8a79fb89fc897df10dc0324
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-09 17:22:22 +0300

    m

--- app/Console/Commands/AskAiCommand.php
+++ app/Console/Commands/AskAiCommand.php
@@ -38,7 +38,7 @@ class AskAiCommand extends Command
     public function handle(): void
     {
         $yourApiKey = config('app.open_ai');
-        dd($yourApiKey);
+        //dd($yourApiKey);
         $model = 'gpt-3.5-turbo';
         $url = 'https://api.openai.com/v1/engines/' . $model . '/completions';
 
