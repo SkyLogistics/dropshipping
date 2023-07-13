fa5390bc530e5531608840dbba29c52ad996cebd
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-13 15:07:59 +0300

    =

--- app/Console/Commands/AskAiCommand.php
+++ app/Console/Commands/AskAiCommand.php
@@ -119,7 +119,7 @@ class AskAiCommand extends Command
         $prompts = OrigamiProducts::query()
             ->where('provider', 'royal')
             ->where('name', '!=', '')
-            ->where('id','>', 32015)
+            ->where('id','>', 32075)
             ->get();
 
         if ($prompts) {
